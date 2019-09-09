f22d167c631bfec0c1c0620f435ca2c09677ee22
孙梦涵 <33645045+sunmenghan2017@users.noreply.github.com>
2019-09-09 14:23:14 +0800

    Update bombTimer.js

--- （2）NodeJS全局API/程序②③④⑤⑥⑦/bombTimer.js
+++ （2）NodeJS全局API/程序②③④⑤⑥⑦/bombTimer.js
@@ -9,3 +9,17 @@ var bomb1 = new Bomb("bomb!!!");
 setTimeout(function(){
     bomb1.explode();
 },2000);
+
+//以下为修正版
+/**
+ * 
+function Bomb (message) {
+    this.message="bomb!!!";
+}
+Bomb.protoType.explode=function(){
+    console.log(this);
+        console.log(this.message);
+}
+var bomb=new Bomb();
+setTimeout(bomb.explode.bind(bomb),2000);
+ */
