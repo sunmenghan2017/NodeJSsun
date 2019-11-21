294d833beb46c7bd6029bd10f013a49db29732dc
孙梦涵 <33645045+sunmenghan2017@users.noreply.github.com>
2019-11-21 09:38:04 +0800

    Update dog.js

--- （3）NodeJS事件/2019-9-11.2/dog.js
+++ （3）NodeJS事件/2019-9-11.2/dog.js
@@ -12,3 +12,33 @@ function Dog(name,energy){
 //2.子构造函数继承父构造函数prototype上面的相关方法
 Dog.prototype.__proto__=EventEmitter.prototype;
 module.exports=Dog;//对外公布
+
+// const EventEmitter = require('events').EventEmitter;
+
+// function Dog(name, energy) {
+//   var _name, _energy;
+//   var that = this;
+
+//   EventEmitter.call(this);
+//   _name = name;
+//   _energy = energy;
+
+//   var timer = setInterval(() => {
+//     if(energy > 0) {
+//       that.emit('bark');
+//       _energy--;
+//     }
+
+//     if(_energy < 0) {
+//       global.clearInterval(timer);
+//     }
+//   }, 1000);
+
+//   this.name = () => _name;
+    
+//   this.energy = () => _energy;
+// }
+
+// Dog.prototype = EventEmitter.prototype;
+
+// module.exports = Dog;
